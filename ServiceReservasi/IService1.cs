2cf103e403f7cd7d6fdab9c3137897c463f867a7
Rayka Agustasya <raykaagustasya16@gmail.com>
2020-12-14 22:08:57 +0700

    pertemuan 11

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -15,6 +15,21 @@ namespace ServiceReservasi
         string GetData(int value);
 
         [OperationContract]
+        string Login(string username, string password);
+
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+
+        [OperationContract]
+        string DeleteRegister(string username);
+
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
+        [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
         // TODO: Add your service operations here
@@ -41,5 +56,18 @@ namespace ServiceReservasi
             get { return stringValue; }
             set { stringValue = value; }
         }
+
+    }
+
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 }
--- ServiceReservasi/Service1.cs
+++ ServiceReservasi/Service1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
@@ -10,6 +11,10 @@ namespace ServiceReservasi
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
+        private SqlConnection connection;
+        private SqlCommand com;
+        private string constring;
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
@@ -27,5 +32,42 @@ namespace ServiceReservasi
             }
             return composite;
         }
+
+        public string Login(string username, string password)
+        {
+            string kategori = "";
+            
+            string sql = "Select Kategori from Login where Username='" + username + "' and Password= '" + password "'";
+            connection = new SqlConnection(constring);
+            com = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = com.ExecuteReader(0);
+            while (reader.GetString())
+            {
+                kategori = reader.GetString(0);
+            }
+
+            return kategori;
+        }
+
+        public string Register(string username, string password, string kategori)
+        {
+            try
+            {
+                string sql = "insert into Login values ('" + username + "',  '" + password + "', '" + kategori + "')";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+        public string UpdateRegister(string username)
     }
 }
